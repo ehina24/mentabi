3b893e99d8f2edbc9a72479ce2c55eb572d40774
ehina24 <2240019@ecc.ac.jp>
2025-12-02 14:43:01 +0900

    create　トップページ作成

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>めんたび</title>
+</head>
+<body>
+    <thead>
+        <nav>
+            <img src="./img/logo.svg" alt="サイトロゴ">
+            <a href="">ログイン</a>
+        </nav>
+    </thead>
+
+    <tbody>
+        <img src="./img/noren.svg" alt="暖簾">
+        <h1>三つの系統、それぞれの個性と魅力<br>和歌山ラーメンの味の違いを、見て、知って、味わおう。</h1>
+
+        <nav>
+            <button>車庫前系</button>
+            <button>井出系</button>
+            <button>独自系</button>
+        </nav>
+
+        <h2>香り立つ醤油、すっきり豚骨。<br>車庫前系の魅力</h2>
+        <p>和歌山ラーメンの原点ともいわれる「車庫前系」。
+            その名は、かつて和歌山駅近くの国鉄(現JR)車庫前に
+            並んだ屋台に由来します。醤油豚骨をベースにした透き通る
+            あっさりしたスープは、醤油の香ばしさと豚骨の旨みが調和。
+            シンプルな具材と優しい味わいで、地元に長く愛され続けています。
+        </p>
+        <img src="./img/gradation_bar.svg" alt="グラデーションバー">
+        <img src="" alt="">
+
+        <h3>あなたにピッタリのラーメンを診断しよう！</h3>
+        <button>診断する</button>
+    </tbody>
+</body>
+</html>
