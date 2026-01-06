4ef62c7c2a73b12c4ac70384c482c52f287a9be4
ehina24 <2240019@ecc.ac.jp>
2026-01-06 14:22:03 +0900

    add　ログインページスタイル追加

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/login.css">
+    <title>めんたび</title>
+</head>
+<body>
+    <img src="./img/logo_big.svg" alt="サイトロゴ" class="logo">
+    <div class="inputWrap">
+        <input type="email" placeholder="メールアドレス" class="mail">
+        <input type="password" placeholder="パスワード" class="pass">
+
+        <a href="">パスワードを忘れた方はこちら</a>
+
+        <button class="loginBtn" onclick="location.href='index.php'">ログイン</button>
+    </div>
+</body>
+</html>
